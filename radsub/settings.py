33dc7fb2105f8255f8728efd5bcddd21bab9fc11
Lindsay Silver <lgsilver@gmail.com>
2013-04-15 17:06:35 -0700

    Fixes

--- radsub/settings.py
+++ radsub/settings.py
@@ -27,22 +27,22 @@ AWS_QUERYSTRING_AUTH = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = 'http://lindsay-gs.s3-website-us-east-1.amazonaws.com/uploads/'
+MEDIA_ROOT = 'http://lindsay-gs.s3.amazonaws.com/uploads/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://lindsay-gs.s3-website-us-east-1.amazonaws.com/uploads/'
+MEDIA_URL = 'http://lindsay-gs.s3.amazonaws.com/uploads/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'http://lindsay-gs.s3-website-us-east-1.amazonaws.com/'
+STATIC_ROOT = 'http://lindsay-gs.s3.amazonaws.com/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://lindsay-gs.s3-website-us-east-1.amazonaws.com/'
+STATIC_URL = 'http://lindsay-gs.s3.amazonaws.com/'
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
