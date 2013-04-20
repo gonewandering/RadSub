f8ff220427abe86af98c0addddfa0a5feefd5318
Lindsay Silver <lgsilver@gmail.com>
2013-04-19 17:02:00 -0700

    Fixed menu size

--- radsub/settings.py
+++ radsub/settings.py
@@ -27,24 +27,24 @@ AWS_QUERYSTRING_AUTH = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = 'http://lindsay-gs.s3.amazonaws.com/uploads/'
+MEDIA_ROOT = 'uploads/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://lindsay-gs.s3.amazonaws.com/uploads/'
+MEDIA_URL = 'http://lindsay-gs.s3.amazonaws.com/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'http://lindsay-gs.s3.amazonaws.com/'
+STATIC_ROOT = 'media/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = 'http://lindsay-gs.s3.amazonaws.com/'
 
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+ADMIN_MEDIA_PREFIX = 'admin/'
 
 
 # Change Static Files and Media to S3
@@ -96,7 +96,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "media/",
+    "",
 )
 
 # List of finder classes that know how to find static files in
